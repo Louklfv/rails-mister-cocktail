b768c6f56cf47b78842509996a946ca055efa0e2
Louka Lefeuvre <louka.lefeuvre@gmail.com>
2020-11-13 16:36:23 +0100

    Style added

---
+++ app/javascript/channels/search.js
@@ -0,0 +1,14 @@
+// const initSearch = () => {
+//   const search = document.querySelector(".cards");
+//   console.log(search)
+//   // const cards = document.querySelector(".row justify-content-between");
+//   // console.log(cards);
+//   console.log("hello");
+// };
+
+// // search.addEventListener('submit', (event) => {
+// //   event.preventDefault();
+
+// // })
+
+// export { initSearch };
